a458145107d92ab2766f5cfb7a3f6c3688a1c360
N1ght420 <n1ghtpe0ple@protonmail.com>
2019-12-04 07:24:01 +0700

    Update code

--- shodan.sh
+++ shodan.sh
@@ -8,11 +8,9 @@ lightgreen='\e[1;32m'
 white='\e[1;37m'
 red='\e[1;31m'
 yellow='\e[1;33m'
-
-clear
-
 myip=`shodan myip`
 
+clear
 echo -e "$red"
 echo -e " _______  __   __  _______  ______   _______  __    _ "
 echo -e "|       ||  | |  ||       ||      | |   _   ||  |  | |"
@@ -31,10 +29,12 @@ echo -e ""
 read -p "  ~ Select : " act;
 if [ $act == '1' ]
 then
-    read -p "  ~ Search : " target;
-    shodan search --fields ip_str,port,hostnames $target
+    read -p "  ~ Search : " query;
+    shodan search --fields ip_str,port,hostnames $query
 elif [ $act == '2' ]
 then
-    read -p "  ~ IP : " target;
-    shodan host $target
+    read -p "  ~ IP : " ip;
+    shodan host $ip
+else
+    exit
 fi
